45625d3a1775be8a8d968dc17e1f3ac620f4a99e
lolcese <lolcese@gmail.com>
2021-08-08 22:04:48 -0300

    Ignora errores en decode

--- monitoreo_juegos.py
+++ monitoreo_juegos.py
@@ -31,7 +31,7 @@ def baja_pagina(url):
         data = urllib.request.urlopen(req).read()
     except HTTPError as e:
         return "Error"
-    return data.decode('unicode_escape').encode('utf-8')
+    return data.decode('unicode_escape', errors = 'ignore')
 
 ######### Lee información de BLAM
 def lee_pagina_blam(ju_id):
