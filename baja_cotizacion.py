089f1714433ca741dbf7fb725a9cd228cb50b9bc
lolcese <lolcese@gmail.com>
2021-08-16 15:55:24 -0300

    Separa bajada de ranking y cotización

--- baja_cotizacion.py
+++ baja_cotizacion.py
@@ -6,7 +6,6 @@ import urllib.request
 import os
 import constantes
 import path
-import time
 from datetime import datetime
 import requests
 
@@ -57,31 +56,5 @@ if abs(euro - euro_viejo) / euro_viejo > 0.05:
 else:
     cursor.execute('UPDATE variables SET valor = ?, fecha = ? WHERE variable = "euro"',(euro, fecha))
     conn.commit()
-
-######### Baja ranking de BGG
-cursor.execute('SELECT DISTINCT BGG_id FROM juegos')
-juegos_BGG = cursor.fetchall()
-for j in juegos_BGG:
-    votos = {}
-    BGG_id = j[0]
-    url = f'https://api.geekdo.com/xmlapi2/thing?id={BGG_id}&stats=1'
-    req = urllib.request.Request(url,headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'}) 
-    data = urllib.request.urlopen(req).read()
-    data = data.decode('utf-8')
-    ranking = html.unescape(re.search('name=\"boardgame\".*?value=\"(.*?)\"',data)[1])
-
-    votos_dep = float(re.search('poll name=\"language_dependence\".*?totalvotes=\"(.*?)\"',data)[1])
-    if votos_dep >= 3:
-        votos[1] = float(re.search('result level.*? value=\"No necessary in-game text\" numvotes=\"(.*?)\"',data)[1])
-        votos[2] = float(re.search('result level.*? value=\"Some necessary text - easily memorized or small crib sheet\" numvotes=\"(.*?)\"',data)[1])
-        votos[3] = float(re.search('result level.*? value=\"Moderate in-game text - needs crib sheet or paste ups\" numvotes=\"(.*?)\"',data)[1])
-        votos[4] = float(re.search('result level.*? value=\"Extensive use of text - massive conversion needed to be playable\" numvotes=\"(.*?)\"',data)[1])
-        votos[5] = float(re.search('result level.*? value=\"Unplayable in another language\" numvotes=\"(.*?)\"',data)[1])
-        dependencia_leng = int(max(votos, key=votos.get))
-    else:
-        dependencia_leng = 0
-    cursor.execute('UPDATE juegos SET ranking = ?, dependencia_leng = ? WHERE BGG_id = ?',(ranking, dependencia_leng, BGG_id))
-    conn.commit()
-    time.sleep(3)
 
 cursor.close()
