ca7700bcb7ec14932cda0b865d42eb6e7d18ce36
lolcese <lolcese@gmail.com>
2021-11-04 21:25:53 -0300

    Cambniado el header

--- monitoreo_juegos.py
+++ monitoreo_juegos.py
@@ -31,7 +31,7 @@ updater = Updater(bot_token)
 
 ######### Baja una página cualquiera
 def baja_pagina(url):
-    req = urllib.request.Request(url,headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'}) 
+    req = urllib.request.Request(url,headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'}) 
     try:
         data = urllib.request.urlopen(req)
     except HTTPError as e:
