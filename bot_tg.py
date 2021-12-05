464b6495ab60a5ac8b374f92f7a0f3203c994ee2
lolcese <lolcese@gmail.com>
2021-12-05 11:22:58 -0300

    Bug en alarmas de ofertas

--- bot_tg.py
+++ bot_tg.py
@@ -797,8 +797,14 @@ def mensaje_oferta(update: Update, context: CallbackContext) -> int:
     else:
         conn = conecta_db()
         cursor = conn.cursor()
-        cursor.execute('UPDATE alarmas_ofertas SET tipo_alarma = ? WHERE id_usuario = ?',[tipo_of, usuario_id])
-        conn.commit()
+        cursor.execute('SELECT id_usuario, tipo_alarma FROM alarmas_ofertas WHERE id_usuario = ?',[usuario_id])
+        alarmas_ofertas = cursor.fetchone()
+        if alarmas_ofertas == None:
+            cursor.execute('INSERT INTO alarmas_ofertas (tipo_alarma, id_usuario)',[tipo_of, usuario_id])
+            conn.commit()
+        else:
+            cursor.execute('UPDATE alarmas_ofertas SET tipo_alarma = ? WHERE id_usuario = ?',[tipo_of, usuario_id])
+            conn.commit()
     keyboard = [
         [InlineKeyboardButton("\U00002B06 Inicio", callback_data='inicio')],
     ]
