a19950dcd944162de433abb7e13d161234aaeba1
lolcese <lolcese@gmail.com>
2022-01-08 19:23:13 -0300

    test

--- bot_tg.py
+++ bot_tg.py
@@ -262,6 +262,7 @@ def juegos_stockalfab_sitio(update: Update, context: CallbackContext) -> int:
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     texto_mensaje_div = dividir_texto(f"{texto}\n", 50)
+    print(texto_mensaje_div)
     for t in texto_mensaje_div[0:-2]:
         context.bot.send_message(chat_id = usuario_id, text = t, parse_mode = "HTML", disable_web_page_preview = True)
     context.bot.send_message(chat_id = usuario_id, text = texto_mensaje_div[-1], parse_mode = "HTML", reply_markup=reply_markup, disable_web_page_preview = True)
