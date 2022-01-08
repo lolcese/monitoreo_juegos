4ebba40fca1fb5cd5b38b9d08b3d32d57472cbf2
lolcese <lolcese@gmail.com>
2022-01-08 19:48:38 -0300

    -1 al len en divide

--- bot_tg.py
+++ bot_tg.py
@@ -36,7 +36,7 @@ def conecta_db():
 def dividir_texto(texto, n):
     lineas = texto.split("\n")
     bloque = []
-    for i in range(0, len(lineas), n):
+    for i in range(0, len(lineas)-1, n):
         bloque.append("\n".join(lineas[i:i + n]))
     return bloque
 
@@ -262,7 +262,6 @@ def juegos_stockalfab_sitio(update: Update, context: CallbackContext) -> int:
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     texto_mensaje_div = dividir_texto(f"{texto}\n", 20)
-    print(texto_mensaje_div[-1])
     for t in texto_mensaje_div[0:-2]:
         context.bot.send_message(chat_id = usuario_id, text = t, parse_mode = "HTML", disable_web_page_preview = True)
     context.bot.send_message(chat_id = usuario_id, text = texto_mensaje_div[-1], parse_mode = "HTML", reply_markup=reply_markup, disable_web_page_preview = True)
