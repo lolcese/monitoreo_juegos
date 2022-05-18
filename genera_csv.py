0b50693383e010debe36d9d8153c614628797077
Luis Olcese <lolcese@gmail.com>
2022-05-18 14:22:43 -0300

    Optimización de la generación de csv

--- genera_csv.py
+++ genera_csv.py
@@ -24,25 +24,21 @@ def main():
     ju = open(constantes.exporta_file, mode='w', newline='', encoding="UTF-8")
     juegos_exporta = csv.writer(ju, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-    cursor.execute('SELECT nombre, BGG_id, id_juego, sitio, sitio_ID, dependencia_leng FROM juegos ORDER BY nombre')
+    cursor.execute('SELECT nombre, BGG_id, sitio, sitio_ID, dependencia_leng, precio_actual, fecha_mejor, precio_mejor FROM juegos ORDER BY nombre')
     juegos_id = cursor.fetchall()
     for j in juegos_id:
-        nombre, BGG_id, id_juego, sitio, sitio_ID, dependencia_leng = j
-        cursor.execute('SELECT precio, fecha FROM precios WHERE id_juego = ? ORDER BY fecha DESC LIMIT 1', [id_juego])
-        dat = cursor.fetchone()
-        if dat:
-            precio_actual, fecha = dat
-            if precio_actual == None:
-                precio = "-"
-            else:
-                precio = f"${precio_actual:.0f}"
-            cursor.execute('SELECT precio FROM precios WHERE id_juego = ? AND precio NOT NULL AND (fecha BETWEEN datetime("now", "-15 days", "localtime") AND datetime("now", "localtime")) ORDER BY precio ASC LIMIT 1', [id_juego])
-            min_precio = cursor.fetchone()
-            if min_precio:
-                min_precio = f"${min_precio[0]:.0f}"
-            else:
-                min_precio = "-"
-            juegos_exporta.writerow([nombre,constantes.sitio_URL['BGG']+str(BGG_id),constantes.sitio_nom[sitio],constantes.sitio_URL[sitio]+sitio_ID, precio, fecha, min_precio, constantes.dependencia_len[dependencia_leng]])
+        nombre, BGG_id, sitio, sitio_ID, dependencia_leng, precio_actual, min_fecha, precio_min = j
+        if precio_actual == None:
+            precio = "-"
+        else:
+            precio = f"${precio_actual:.0f}"
+        if precio_min == None:
+            min_precio = "-"
+        else:
+            min_precio = f"${min_precio[0]:.0f}"
+        if min_fecha == None:
+            min_fecha = "-"
+        juegos_exporta.writerow([nombre,constantes.sitio_URL['BGG']+str(BGG_id),constantes.sitio_nom[sitio],constantes.sitio_URL[sitio]+sitio_ID, precio, min_fecha, min_precio, constantes.dependencia_len[dependencia_leng]])
     
     ju.close()
     if os.path.exists(f'graficos/{constantes.exporta_file}'):
