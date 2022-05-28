fa8db05d80f5a6330df76024f8c3262bfcd7c213
Luis Olcese <lolcese@gmail.com>
2022-05-28 19:05:16 -0300

    path correcto

--- path.py
+++ path.py
@@ -2,5 +2,4 @@
 ##### path
 ########################################################
 
-actual = "c:/Datos/Luis/bgg/monitoreo_juegos"
-# actual = "/home/lolcese/monitoreo_juegos"
+actual = "/home/lolcese/monitoreo_juegos"
