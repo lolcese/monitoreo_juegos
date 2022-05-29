ce81c34cd8a3d98ca41245fa876419040299359c
Luis Olcese <lolcese@gmail.com>
2022-05-29 20:17:49 -0300

    No borra la alarma al tener error enviando mensaje

--- manda.py
+++ manda.py
@@ -18,23 +18,23 @@ def send_message(chat_id, text):
     try:
         bot.send_message(chat_id = chat_id, text = text, parse_mode = "HTML", disable_web_page_preview = True)
     except:
-        conn = sqlite3.connect(constantes.db_file, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
-        conn.execute("PRAGMA journal_mode=WAL")
-        cursor = conn.cursor()
-        cursor.execute('DELETE FROM alarmas WHERE id_persona = ?',[chat_id])
-        cursor.execute('DELETE FROM alarmas_ofertas WHERE id_usuario = ?',[chat_id])
-        print(f"{datetime.now()} - Error enviando mensaje a {chat_id} - Borrado")
-        conn.commit()
+        # conn = sqlite3.connect(constantes.db_file, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+        # conn.execute("PRAGMA journal_mode=WAL")
+        # cursor = conn.cursor()
+        # cursor.execute('DELETE FROM alarmas WHERE id_persona = ?',[chat_id])
+        # cursor.execute('DELETE FROM alarmas_ofertas WHERE id_usuario = ?',[chat_id])
+        print(f"{datetime.now()} - Error enviando mensaje a {chat_id}")
+        # conn.commit()
 
 def send_photo(chat_id, caption, photo):
     bot = get_bot()
     try:
-        bot.send_photo(chat_id, photo = open(photo, 'rb'), caption = caption, parse_mode = "HTML", disable_web_page_preview = True)
+        bot.send_photo(chat_id, photo = open(photo, 'rb'), caption = caption, parse_mode = "HTML")
     except:
-        conn = sqlite3.connect(constantes.db_file, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
-        conn.execute("PRAGMA journal_mode=WAL")
-        cursor = conn.cursor()
-        cursor.execute('DELETE FROM alarmas WHERE id_persona = ?',[chat_id])
-        cursor.execute('DELETE FROM alarmas_ofertas WHERE id_usuario = ?',[chat_id])
+        # conn = sqlite3.connect(constantes.db_file, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+        # conn.execute("PRAGMA journal_mode=WAL")
+        # cursor = conn.cursor()
+        # cursor.execute('DELETE FROM alarmas WHERE id_persona = ?',[chat_id])
+        # cursor.execute('DELETE FROM alarmas_ofertas WHERE id_usuario = ?',[chat_id])
         print(f"{datetime.now()} - Error enviando imagen a {chat_id} - Borrado")
-        conn.commit()
+        # conn.commit()
