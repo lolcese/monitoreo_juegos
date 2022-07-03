b37cd1c73d6a9f575accf2bf39b4afcadaaee8c5
Luis Olcese <lolcese@gmail.com>
2022-07-03 12:33:46 -0300

    Cambio en backup

--- backup.sh
+++ backup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 d=$(date +%Y-%m-%d)
 sqlite3 db/monitoreo_juegos.db ".timeout 100000" ".backup 'db/monitoreo_juegos_$d.db'"
-tar cvfz db/monitoreo_juegos_$d.tar.gz monitoreo_juegos_$d.db db/monitoreo_juegos_todo.db
-#rclone copy db/monitoreo_juegos_$d.tar.gz gdrive:monitoreo_juegos_backup
-#rm db/monitoreo_juegos_$d.db
-#rm db/monitoreo_juegos_$d.tar.gz
+tar cvfz db/monitoreo_juegos_$d.tar.gz db/monitoreo_juegos_$d.db db/monitoreo_juegos_todo.db
+rclone copy db/monitoreo_juegos_$d.tar.gz gdrive:monitoreo_juegos_backup
+rm -f db/monitoreo_juegos_$d.db
+rm -f db/monitoreo_juegos_$d.tar.gz
 
 sqlite3 << EOF
 ATTACH 'db/monitoreo_juegos.db' as db1;
