d49bdd92a4c7a42fe82ad8d3aadc3273006d9ee7
Luis Olcese <lolcese@gmail.com>
2022-07-16 15:55:34 -0300

    Saca la excepción al envío

--- manda.py
+++ manda.py
@@ -11,24 +11,24 @@ def get_bot():
 
 def send_message(chat_id, text):
     bot = get_bot()
-    try:
-        bot.send_message(chat_id = chat_id, text = text, parse_mode = "HTML", disable_web_page_preview = True)
-    except:
-        pass
+    # try:
+    bot.send_message(chat_id = chat_id, text = text, parse_mode = "HTML", disable_web_page_preview = True)
+    # except:
+    #     pass
         # print(f"{datetime.now()} - Error enviando mensaje a {chat_id}")
 
 def send_photo(chat_id, caption, photo):
     bot = get_bot()
-    try:
-        bot.send_photo(chat_id, photo = open(photo, 'rb'), caption = caption, parse_mode = "HTML")
-    except:
-        pass
+    # try:
+    bot.send_photo(chat_id, photo = open(photo, 'rb'), caption = caption, parse_mode = "HTML")
+    # except:
+        # pass
         # print(f"{datetime.now()} - Error enviando imagen a {chat_id}")
 
 def send_message_key(chat_id, text, reply_markup):
     bot = get_bot()
-    try:
-        bot.send_message(chat_id = chat_id, text = text, parse_mode = "HTML", disable_web_page_preview = True, reply_markup = reply_markup)
-    except:
-        pass
+    # try:
+    bot.send_message(chat_id = chat_id, text = text, parse_mode = "HTML", disable_web_page_preview = True, reply_markup = reply_markup)
+    # except:
+    #     pass
         # print(f"{datetime.now()} - Error enviando mensaje a {chat_id}")
