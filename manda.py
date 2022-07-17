6802bad1397ff0f2aa401391cadab56a89c595bb
Luis Olcese <lolcese@gmail.com>
2022-07-17 09:00:47 -0300

    Diferente mensaje de error en cada caso

--- manda.py
+++ manda.py
@@ -15,7 +15,7 @@ def send_message(chat_id, text):
         bot.send_message(chat_id = chat_id, text = text, parse_mode = "HTML", disable_web_page_preview = True)
     except Exception as e:
         if 'Forbidden: bot was blocked by the user' not in e.message and 'Forbidden: user is deactivated' not in e.message: 
-            print(f"Fecha: {datetime.now()}: {chat_id}\n\n{text}\n\nError: {e}")
+            print(f"Fecha: {datetime.now()}\nid: {chat_id}\nMensaje: {text}\nError: {e}")
         # print(f"{datetime.now()} - Error enviando mensaje a {chat_id}")
 
 def send_photo(chat_id, caption, photo):
@@ -24,7 +24,7 @@ def send_photo(chat_id, caption, photo):
         bot.send_photo(chat_id, photo = open(photo, 'rb'), caption = caption, parse_mode = "HTML")
     except Exception as e:
         if 'Forbidden: bot was blocked by the user' not in e.message and 'Forbidden: user is deactivated' not in e.message: 
-            print(f"Fecha: {datetime.now()}: {chat_id}\n\n{caption}\n\nError: {e}")
+            print(f"Fecha: {datetime.now()}\nid: {chat_id}\nMensaje con foto: {caption}\nError: {e}")
         # print(f"{datetime.now()} - Error enviando imagen a {chat_id}")
 
 def send_message_key(chat_id, text, reply_markup):
@@ -33,5 +33,5 @@ def send_message_key(chat_id, text, reply_markup):
         bot.send_message(chat_id = chat_id, text = text, parse_mode = "HTML", disable_web_page_preview = True, reply_markup = reply_markup)
     except Exception as e:
         if 'Forbidden: bot was blocked by the user' not in e.message and 'Forbidden: user is deactivated' not in e.message: 
-            print(f"Fecha: {datetime.now()}: {chat_id}\n\n{text}\n\nError: {e}")
+            print(f"Fecha: {datetime.now()}\nid: {chat_id}\nMensaje con botón: {text}\nError: {e}")
         # print(f"{datetime.now()} - Error enviando mensaje a {chat_id}")
